1934a4b564fde9ff95909a976965601ba67f8737
marcus.augusto <marcus.augusto@globalsys.com.br>
2024-03-05 10:03:50 -0300

    basic ui design

---
+++ frontend/src/components/home/Sidebar/LogoutButton.tsx
@@ -0,0 +1,13 @@
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+      <div className="text-white cursor-pointer">
+        <BiLogOut size={24} />
+      </div>
+    </div>
+  );
+};
+
+export default LogoutButton;
---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import MessagesContainer from "../../components/home/MessagesContainer";
+import Sidebar from "../../components/home/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessagesContainer />
+    </div>
+  );
+};
+
+export default Home;
