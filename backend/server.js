6a480a0e20117810a419b4c2a7ef7fa6e16cd246
marcus.augusto <marcus.augusto@globalsys.com.br>
2024-03-07 13:01:14 -0300

    scripts adjustments

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -20,9 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
